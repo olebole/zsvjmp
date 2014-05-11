e27cf832d17eb8993f19d5b2b3f7c3aea3333ab7
Ole Streicher <debian@liska.ath.cx>
2014-05-11 17:23:34 +0200

    Revert to (mostly identical) original version

--- zsvjmp-powerpc.s
+++ zsvjmp-powerpc.s
@@ -1,19 +1,16 @@
 	.file	"zsvjmp.s"
 
-# Copyright (c) 2014 Ole Streicher <debian@liska.ath.cx>
-# Distributable under the same license as IRAF
-# This file contains the Linux Powerpc version of ZSVJMP for Debian.
-
 	.section	".text"
 	.align 2
 	.globl zsvjmp_
 	.type	zsvjmp_, @function
 zsvjmp_:
-	li 9,0          #
-	stw 9,0(4)      # *status = 0
+	# R3 = buf, R4 = &status
+	li 11,0         #
+	stw 11,0(4)     # *status = 0
 	stw 4,0(3)      # buf[0] = status
 	addi 3,3,4      # &buf[1] --> 1st arg for sigsetjmp
 	li 4,0          # 0       --> 2nd arg for sigsetjmp
-	b __sigsetjmp
+	b __sigsetjmp@PLT
 	.size	zsvjmp_,.-zsvjmp_
 	.section	.note.GNU-stack,"",@progbits
